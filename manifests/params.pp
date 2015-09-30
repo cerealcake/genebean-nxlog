90cbae21d0a19a811f8bd460df5b3ae4556a7561
Jaime Viloria <jaimeviloria@riplife.es>
2015-09-30 15:27:14 +0200

    added possibility for extra options on input

--- manifests/params.pp
+++ manifests/params.pp
@@ -8,6 +8,7 @@ class nxlog::params {
   $input_execs       = []
   $input_module      = undef
   $input_file_path   = undef
+  $input_options     = undef
   $nxlog_root        = undef
   $output_address    = undef
   $output_file_path  = undef
